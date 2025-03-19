e2e2fef4e5710dd7f76f4bca23ac9579d100d85b
Cesar Coutino <cou23001@byui.edu>
2025-03-19 12:15:32 -0700

    instructor

---
+++ models/instructor.js
@@ -0,0 +1,16 @@
+// models/instructor.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const instructorSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Instructor', instructorSchema);
