49749475ca7368df8e61ca2ca57a46eec3d88a4d
Cesar Coutino <cou23001@byui.edu>
2025-03-19 18:21:56 -0700

    update instructor

--- controllers/instructorController.js
+++ controllers/instructorController.js
@@ -1,5 +1,9 @@
 // controllers/instructorController.js
-const Instructor = require('../models/instructor');
+const Instructor = require("../models/instructor");
+const User = require("../models/user");
+const mongoose = require("mongoose");
+const argon2 = require("argon2");
+const UserRole = require("../models/userRole");
 
 /**
  * @swagger
@@ -27,12 +31,15 @@ const Instructor = require('../models/instructor');
 const getInstructors = async (req, res) => {
   try {
     const instructors = await Instructor.find();
+    if (instructors.length === 0) {
+      return res.status(404).send("Instructors not found");
+    }
     res.json(instructors);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
-}
+};
 
 /**
  * @swagger
@@ -54,18 +61,78 @@ const getInstructors = async (req, res) => {
  *             schema:
  *               $ref: '#/components/schemas/Instructor'
  *       400:
- *         description: Bad request
+ *         description: Bad request (e.g., invalid input, duplicate email)
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Invalid input"
+ *       500:
+ *         description: Internal server error
+ *     security:
+ *       - BearerAuth: [] # Add if authentication is required
  */
 const createInstructor = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
-    const instructor = new Instructor(req.body);
-    await instructor.save();
+    const { firstName, lastName, email, password, role="instructor" } = req.body;
+    // Validate input
+    if (!firstName || !lastName || !email || !password) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email }).session(session);
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already registered' });
+    }
+
+    // Get the user role
+    const userRole = await UserRole.findOne({ name: role });
+    if (!userRole) {
+      return res.status(400).json({ error: 'Invalid role' });
+    }
+
+    // Hash the password
+    const hashedPassword = await argon2.hash(password);
+
+    // Create the user
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      role: userRole._id,
+    });
+    await user.save({ session });
+
+    // Create the instructor
+    const instructor = new Instructor({ userId: user._id });
+    await instructor.save({ session });
+
+    // Commit the transaction
+    await session.commitTransaction();
+    session.endSession();
+
+    // Respond with the instructor
     res.status(201).json(instructor);
   } catch (error) {
+    // Abort the transaction on error
+    await session.abortTransaction();
+    session.endSession();
+
     console.error(error);
-    res.status(400).send('Bad request');
+    if (error.name === 'ValidationError') {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: 'Internal server error' });
+    }
   }
-}
+};
 
 /**
  * @swagger
@@ -94,14 +161,14 @@ const getInstructorById = async (req, res) => {
   try {
     const instructor = await Instructor.findById(req.params.id);
     if (!instructor) {
-      res.status(404).send('The instructor was not found');
+      res.status(404).send("The instructor was not found");
     }
     res.json(instructor);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
-}
+};
 
 /**
  * @swagger
@@ -134,20 +201,20 @@ const getInstructorById = async (req, res) => {
  */
 const updateInstructor = async (req, res) => {
   try {
-    const instructor = await Instructor.findByIdAndUpdate
-      (req
-        .params
-        .id, req.body, {new: true});
+    const instructor = await Instructor.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     if (!instructor) {
-      res.status(404).send('The instructor was not found');
+      res.status(404).send("The instructor was not found");
     }
     res.json(instructor);
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
-}
+};
 
 /**
  * @swagger
@@ -172,19 +239,19 @@ const deleteInstructor = async (req, res) => {
   try {
     const instructor = await Instructor.findByIdAndDelete(req.params.id);
     if (!instructor) {
-      res.status(404).send('The instructor was not found');
+      res.status(404).send("The instructor was not found");
     }
     res.json(instructor);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
-}
+};
 
 module.exports = {
   getInstructors,
   createInstructor,
   getInstructorById,
   updateInstructor,
-  deleteInstructor
+  deleteInstructor,
 };
