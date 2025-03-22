1f323f89126c2255709de98b0407ad6d64673753
Jacob <jake_76847@hotmail.com>
2025-03-22 13:18:28 -0400

    Auth

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -5,18 +5,18 @@ const { getGroups, getGroup, createGroup, updateGroup, deleteGroup } = require('
 const authenticate = require('../middleware/authenticate');
 
 // GET /groups
-router.get('/groups', getGroups);
+router.get('/groups', authenticate, getGroups);
 
 // GET /groups/:id
-router.get('/groups/:id', getGroup);
+router.get('/groups/:id', authenticate, getGroup);
 
 // POST /groups
-router.post('/groups', createGroup);
+router.post('/groups', authenticate, createGroup);
 
 // PUT /groups/:id
-router.put('/groups/:id', updateGroup);
+router.put('/groups/:id', authenticate, updateGroup);
 
 // DELETE /groups/:id
-router.delete('/groups/:id', deleteGroup);
+router.delete('/groups/:id', authenticate, deleteGroup);
 
 module.exports = router;
