64f78a13e3cbfcb56473a003d733e5756f89b5eb
Cesar Coutino <cou23001@byui.edu>
2025-04-03 16:35:32 -0700

    Update doc

--- utils/upload.js
+++ utils/upload.js
@@ -1,4 +1,5 @@
 // utils/upload.js
+// Uploading files to AWS S3 using multer and AWS SDK v3
 const { S3Client, PutObjectCommand, DeleteObjectCommand, HeadObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require("multer");
 const dotenv = require("dotenv");
