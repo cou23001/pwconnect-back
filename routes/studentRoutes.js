aa38e7c5100bf1cfb32379d196eea3242e3c10a5
Cesar Coutino <cou23001@byui.edu>
2025-04-04 23:01:15 -0700

    Add authentication to routes

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -21,53 +21,53 @@ const upload = multer({
   storage: memoryStorage, // Don't save locally
   limits: { fileSize: 2 * 1024 * 1024 }, // Limit file size to 2MB
   fileFilter: (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
     if (!allowedTypes.includes(file.mimetype)) {
-      const error = new Error('Invalid file type');
-      error.code = 'LIMIT_FILE_TYPE';  // Custom error code
+      const error = new Error("Invalid file type");
+      error.code = "LIMIT_FILE_TYPE"; // Custom error code
       return cb(error);
     }
     cb(null, true);
-  }
+  },
 });
 
 // GET /students
-router.get(
-  "/students",
-  authenticate,
-  authorize([10]),
-  getAllStudents
-);
+router.get("/students", authenticate, authorize([10]), getAllStudents);
 
 // GET /students/:id
 // Route requires authentication and authorization for types 1 and 10.
 // It checks if the user owns the data (or is admin).
 router.get(
-  "/students/:id", 
-  authenticate, 
-  authorize([1, 10]), 
-  validateOwnership, 
+  "/students/:id",
+  authenticate,
+  authorize([1, 10]),
+  validateOwnership,
   getStudentById
 );
 
 // POST /students
 router.post(
-  "/students", 
+  "/students",
   upload.single("avatar"),
   uploadErrors,
   authenticate,
   authorize([10]),
-  formDataToJson, createStudent
+  formDataToJson,
+  createStudent
 );
 
 // PUT /students/:id
 // Route requires authentication and authorization for tyoes 1 and 10.
 // It checks if the user owns the data (or is admin).
 router.put(
-  "/students/:id", 
-  upload.single("avatar"), 
+  "/students/:id",
+  upload.single("avatar"),
   uploadErrors,
-  authenticate, authorize([1,10]), validateOwnership, formDataToJson, updateStudent
+  authenticate,
+  authorize([1, 10]),
+  validateOwnership,
+  formDataToJson,
+  updateStudent
 );
 
 // POST /students/upload/:id
@@ -85,10 +85,6 @@ router.put(
 
 // DELETE /students/:id
 // Route requires authentication and authorization for type 10 (admin).
-router.delete("/students/:id", 
-  //authenticate, 
-  //authorize([10]), 
-  deleteStudent
-);
+router.delete("/students/:id", authenticate, authorize([10]), deleteStudent);
 
 module.exports = router;
