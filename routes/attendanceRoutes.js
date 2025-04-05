2198c5f64bd0c61fb25deffa92fd239c6538f2c0
Jacob <jake_76847@hotmail.com>
2025-04-04 22:02:10 -0400

    Adding middleware

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -12,21 +12,21 @@ const {
 const { authenticate } = require('../middleware/authenticate');
 
 // POST /attendance
-router.post('/attendance', createAttendance);
+router.post('/attendance', authenticate, createAttendance);
 
 // GET /attendance
 router.get('/attendance', authenticate, getAttendances);
 
 // GET /attendance/:id
-router.get('/attendance/:id', getAttendance);
+router.get('/attendance/:id', authenticate, getAttendance);
 
 // GET /attendance/:groupId
-router.get('/attendance/group/:groupId', getAttendanceByGroup);
+router.get('/attendance/group/:groupId', authenticate, getAttendanceByGroup);
 
 // PUT /attendance/:id
 router.put('/attendance/:id', authenticate, updateAttendance);
 
 // DELETE /attendance/:id
-router.delete('/attendance/:id', deleteAttendance);
+router.delete('/attendance/:id', authenticate, deleteAttendance);
 
 module.exports = router;
