ad4dcefb8bfef873b1e1e62d7fac92a1f809e716
Cesar Coutino <cou23001@byui.edu>
2025-04-10 22:55:53 -0700

    update term routes

--- routes/termRoutes.js
+++ routes/termRoutes.js
@@ -1,23 +1,23 @@
 // routes/termRoutes.js
 const express = require('express');
 const { getTerms, createTerm, getTermById, updateTerm, deleteTerm } = require('../controllers/termController');
-const { authenticate } = require('../middleware/authenticate');
+const { authenticate, authorize } = require('../middleware/authenticate');
 
 const router = express.Router();
 
 // GET /terms
-router.get('/terms', authenticate, getTerms);
+router.get('/terms', authenticate, authorize([10]), getTerms);
 
 // GET /terms/:id
-router.get('/terms/:id', authenticate, getTermById);
+router.get('/terms/:id', authenticate, authorize([10]), getTermById);
 
 // POST /terms
-router.post('/terms', authenticate, createTerm);
+router.post('/terms', authenticate, authorize([10]), createTerm);
 
 // PUT /terms/:id
-router.put('/terms/:id', authenticate, updateTerm);
+router.put('/terms/:id', authenticate, authorize([10]), updateTerm);
 
 // DELETE /terms/:id
-router.delete('/terms/:id', authenticate, deleteTerm);
+router.delete('/terms/:id', authenticate, authorize([10]), deleteTerm);
 
 module.exports = router;
