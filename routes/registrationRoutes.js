7bf998859c4ffadf0f5a4d5cd8e0fbbaf2cd8965
Cesar Coutino <cou23001@byui.edu>
2025-04-10 22:52:56 -0700

    Upddate authentication and authorize for registration

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -1,24 +1,26 @@
 // routes/registrationRoutes.js
 const express = require('express');
 const { getRegistrations, getRegistrationById, createRegistration, updateRegistration, deleteRegistration, getStudentsByGroupId } = require('../controllers/registrationController');
+const { authorize, authenticate } = require('../middleware/authenticate');
+
 const router = express.Router();
 
 // GET /registrations
-router.get('/registrations', getRegistrations);
+router.get('/registrations', authenticate, authorize([10]), getRegistrations);
 
 // GET /registrations/:id
-router.get('/registrations/:id', getRegistrationById);
+router.get('/registrations/:id', authenticate, authorize([10]), getRegistrationById);
 
 // GET /registrations/group/:groupId/students
-router.get('/registrations/group/:groupId/students', getStudentsByGroupId);
+router.get('/registrations/group/:groupId/students', authenticate, authorize([10]), getStudentsByGroupId);
 
 // POST /registrations
-router.post('/registrations', createRegistration);
+router.post('/registrations', authenticate, authorize([10]), createRegistration);
 
 // PUT /registrations/:id
-router.put('/registrations/:id', updateRegistration);
+router.put('/registrations/:id', authenticate, authorize([10]), updateRegistration);
 
 // DELETE /registrations/:id
-router.delete('/registrations/:id', deleteRegistration);
+router.delete('/registrations/:id', authenticate, authorize([10]), deleteRegistration);
 
 module.exports = router;
